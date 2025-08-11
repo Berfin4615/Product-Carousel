2f1b61977e588b6ba880a61c3fd31ebef37bc6f9
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-08-11 17:52:13 +0300

    A flag has been added to detect whether this is the first time a fetch is being performed. If it is being performed for the first time, data is being fetched.

--- Berfin_Sünnetcioğlu.js
+++ Berfin_Sünnetcioğlu.js
@@ -1,47 +1,70 @@
 (() => {
-    // I add that block because ebebek does not have jquery:
+    // This part downloads jQuery and adds it to the page. If I don't do this, my code won't work because the '$' won't be recognized without jQuery:
     const script = document.createElement('script');
     script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
     script.onload = () => run(window.jQuery);
     document.head.appendChild(script);
-    function run($) {
+
+    // I add a flag to check whether the code is run for the first time or not, this will ensure that it does not fetch twice:
+    window.productListFetched = window.productListFetched || false;
+
+    // Data will be fetched from the link if it has not been fetched before:
+    if (!window.productListFetched) {
+        fetch("https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json")
+            .then(res => res.json())
+            .then(data => {
+                console.log("Product list:", data);
+                window.productListFetched = true; 
+            })
+            .catch(err => console.error("Fetch error:", err));
+    } else {
+        console.log("Daha önce fetch yapıldı. Beğenilen ürünler yükleniyor.");
+    }
     
-    $(() => {
-      (() => {
-        const init = () => {
-          buildHTML();
-          buildCSS();
-          setEvents();
-        };
-
-        const buildHTML = () => {
-          const html = `
-            <div class="container-berfin">
-              <h1>Deneme</h1>
-            </div>
-          `;
-          $('body').append(html);
-        };
-
-        const buildCSS = () => {
-          const css = `
-            .container-berfin {
-              background-color: red;
-              height: 100px;
-              width: 100px;
-            }
-          `;
-          $('<style>').html(css).appendTo('head');
-        };
-
-        const setEvents = () => {
-          $('.container-berfin').on('click', () => {
-            console.log('clicked');
-          });
-        };
-
-        init();
-      })();
-    });
+    // In this section, I will add the HTML, CSS and event codes I need to add:
+    function run($) {
+        $(() => {
+          (() => {
+            const init = () => {
+              buildHTML();
+              buildCSS();
+              setEvents();
+            };
+    
+            const buildHTML = () => {
+              const html = `
+                <div class="banner">
+                    <div class="container-products">
+                        <div class="banner-title">Beğenebileceğinizi düşündüklerimiz</div>
+                        <div class="banner-products"></div>
+                        
+                    </div>
+                </div>
+              `;
+              $('body').append(html);
+            };
+    
+            const buildCSS = () => {
+              const css = `
+                .container-berfin {
+                  background-color: red;
+                  height: 100px;
+                  width: 100px;
+                }
+              `;
+              $('<style>').html(css).appendTo('head');
+            };
+    
+            const setEvents = () => {
+              $('.container-berfin').on('click', () => {
+                console.log('clicked');
+              });
+            };
+    
+            init();
+          })();
+        });
   }
 })();
+
+// TODO!: add 'Çok Satan' and 'Yıldız Ürün' PNG into product img.
