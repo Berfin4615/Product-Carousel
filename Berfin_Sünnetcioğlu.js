36c480eaefbfe9bf4ba16728bf176d0116e6846e
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-08-11 21:02:19 +0300

    Homepage control has been achieved.

--- Berfin_Sünnetcioğlu.js
+++ Berfin_Sünnetcioğlu.js
@@ -5,85 +5,93 @@
     script.onload = () => run(window.jQuery);
     document.head.appendChild(script);
 
-    // I add a flag to check whether the code is run for the first time or not, this will ensure that it does not fetch twice:
-    window.productListFetched = window.productListFetched || false;
+    // Check to see if I'm on the Home Page:
+    if (window.location.pathname === "/") {
+        console.log("You are in the right place!");
+        
+        // I add a flag to check whether the code is run for the first time or not, this will ensure that it does not fetch twice:
+        window.productListFetched = window.productListFetched || false;
+    
+        // Data will be fetched from the link if it has not been fetched before:
+        if (!window.productListFetched) {
+            fetch("https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json")
+                .then(res => res.json())
+                .then(data => {
+                    console.log("Product list:", data);
+                    window.productListFetched = true; 
+                })
+                .catch(err => console.error("Fetch error:", err));
+        } else {
+            console.log("Daha önce fetch yapıldı. Beğenilen ürünler yükleniyor.");
+        }
 
-    // Data will be fetched from the link if it has not been fetched before:
-    if (!window.productListFetched) {
-        fetch("https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json")
-            .then(res => res.json())
-            .then(data => {
-                console.log("Product list:", data);
-                window.productListFetched = true; 
-            })
-            .catch(err => console.error("Fetch error:", err));
-    } else {
-        console.log("Daha önce fetch yapıldı. Beğenilen ürünler yükleniyor.");
-    }
-    
-    // In this section, I will add the HTML, CSS and event codes I need to add:
-    function run($) {
-        $(() => {
-          (() => {
-            const init = () => {
-              buildHTML();
-              buildCSS();
-              setEvents();
-            };
-    
-            const buildHTML = () => {
-              const html = `
-                <div class="banner">
-                    <div class="container-products">
-                        <div class="banner-title-container">
-                            <h2 class="banner-title">Beğenebileceğinizi Düşündüklerimiz</h2>
-                        </div>
-                        <div class="banner-products">
-                            <img />
-                            <div class="product-info"></div>
-                            <div class="product-add-button">Sepete Ekle</div>
+        // In this section, I will add the HTML, CSS and event codes I need to add:
+        function run($) {
+            $(() => {
+              (() => {
+                const init = () => {
+                  buildHTML();
+                  buildCSS();
+                  setEvents();
+                };
+        
+                const buildHTML = () => {
+                  const html = `
+                    <div class="banner">
+                        <div class="container-products">
+                            <div class="banner-title-container">
+                                <h2 class="banner-title">Beğenebileceğinizi Düşündüklerimiz</h2>
+                            </div>
+                            <div class="banner-products">
+                                <img />
+                                <div class="product-info"></div>
+                                <div class="product-add-button">Sepete Ekle</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-              `;
-              $('body').append(html);
-            };
+                  `;
+                  $('body').append(html);
+                };
+        
+                const buildCSS = () => {
+                  const css = `
+                      .banner {
+                          padding: 0px 15px;
+                      }
+                      .banner-title-container {
+                          background-color: #fef6eb;
+                          font-family: Quicksand-Bold;
+                          padding: 25px 67px;
+                          border-top-left-radius: 35px;
+                          border-top-right-radius: 35px;
+                      }
+                      .banner-title {
+                          color: #f28e00;
+                          font-size: 3rem;
+                      }
+                    .product-add-button {
+                      background-color: red;
+                      height: 100px;
+                      width: 100px;
+                    }
+                  `;
+                  $('<style>').html(css).appendTo('head');
+                };
+        
+                const setEvents = () => {
+                  $('.product-add-button').on('click', () => {
+                    console.log('Product Added.');
+                  });
+                };
+        
+                init();
+              })();
+            });
+          }
+    } else {
+        console.log("Wrong Page!");
+    }
     
-            const buildCSS = () => {
-              const css = `
-                  .banner {
-                      padding: 0px 15px;
-                  }
-                  .banner-title-container {
-                      background-color: #fef6eb;
-                      font-family: Quicksand-Bold;
-                      padding: 25px 67px;
-                      border-top-left-radius: 35px;
-                      border-top-right-radius: 35px;
-                  }
-                  .banner-title {
-                      color: #f28e00;
-                      font-size: 3rem;
-                  }
-                .product-add-button {
-                  background-color: red;
-                  height: 100px;
-                  width: 100px;
-                }
-              `;
-              $('<style>').html(css).appendTo('head');
-            };
-    
-            const setEvents = () => {
-              $('.product-add-button').on('click', () => {
-                console.log('Product Added.');
-              });
-            };
-    
-            init();
-          })();
-        });
-  }
 })();
 
 // TODO!: 
